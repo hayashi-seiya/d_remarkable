f6b120659f92fdee74f36f06a34733300fdf8737
Seiya Hayashi <shayashi@hitomedia.jp>
2018-05-26 20:02:05 +0900

    fix rubocop

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,7 +1,7 @@
 class Park < ApplicationRecord
-  has_many :attractions
-  has_many :shows
-  has_many :restaurants
+  has_many :attractions, dependent: :destroy
+  has_many :shows, dependent: :destroy
+  has_many :restaurants, dependent: :destroy
 
   validates :name, presence: true
   validates :business_hours_open, presence: true
