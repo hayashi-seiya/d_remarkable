8042df923f3053b62d1171c3858b6d621c730fd8
Seiya Hayashi <shayashi@hitomedia.jp>
2018-06-03 17:51:42 +0900

    ランドの乗り物をスクレイピングしてデータベースに保存できるように

--- app/models/scraping/disney_land.rb
+++ app/models/scraping/disney_land.rb
@@ -8,7 +8,9 @@ class Scraping::DisneyLand
   end
 
   def initialize
-    @url = "http://disneyreal.asumirai.info/realtime/disneyland-wait-today.html"
+    @land = Park.find_by(name: "land")
+    @base_url = "http://disneyreal.asumirai.info"
+    @index_url = "http://disneyreal.asumirai.info/realtime/disneyland-wait-today.html"
   end
 
   def run
@@ -17,21 +19,54 @@ class Scraping::DisneyLand
 
   private
 
-    attr_reader :url
+    attr_reader :land, :base_url, :index_url
 
     def scraping
-      doc = document_by_html
+      # attractionにまとめて保存する用の配列
+      attraction_params = []
 
-      doc.xpath("//ul[@class='wait-time']//h4").each do |node|
-        puts node.inner_text
+      doc = document_by_index_url
+      attractions = doc.xpath("//ul[@class='wait-time']/li")
+      attractions.each do |attraction|
+        attraction_param = land.attractions.build
+
+        # アトラクション名
+        attraction_param.name = attraction.xpath("a/ul/li[@class='about']/h4").text
+
+        attraction_url = attraction.xpath("a").attribute("href").value # attraction 詳細ページへのリンク
+        attraction_documents = document_by_attraction_url(attraction_url)
+        attraction_documents.xpath("//table[@class='realtime']").each do |attraction_document|
+          # 待ち時間
+          waiting_time = attraction_document.xpath("//td[@class='waittime']").text.strip
+          attraction_param.waiting_time = waiting_time
+
+          # 営業時間
+          # ref: https://goo.gl/HLGbfD
+          business_hours = attraction_document.xpath("//th[contains(text(), '運営時間')]/following-sibling::td[1]").text
+          attraction_param.business_hours_open = business_hours.split("–")[0]
+          attraction_param.business_hours_close = business_hours.split("–")[1]
+        end
+
+        attraction_params << attraction_param
       end
+
+      Attraction.import(attraction_params)
     end
 
-    def document_by_html
-      html = open(url).read
+    # TODO: テスト書き終わったらこのコメントは消す
+    # ラッパーメソッドにしておけばテストの時にスタブ化できそう
+    def document_by_index_url
+      html = open(index_url).read
 
-      # 文字ボケの対策のために強制的にutf-8に変換
+      # 文字化け対策のために強制的にutf-8に変換
       # ref: https://qiita.com/foloinfo/items/435f0409a6e33929ef3c
+      Nokogiri::HTML.parse(html, nil, "utf-8")
+    end
+
+    def document_by_attraction_url(attraction_url)
+      attraction_url.slice!("..") # ../attraction/tl_spmountain.html のようになっているため
+
+      html = open(base_url + attraction_url).read
       Nokogiri::HTML.parse(html, nil, "utf-8")
     end
 end
